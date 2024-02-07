033b814c76e3f897259f2dd08418789d3efe9659
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2024-02-07 18:20:08 -0300

    feat: add save note function, integrated with localStorage + add search notes filter

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -19,7 +23,11 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
-    console.log(content);
+
+    onNoteCreated(content);
+    setContent("");
+    setShouldShowOnBoarding(true);
+
     toast.success("Nota criada com sucesso!");
   }
 
@@ -66,6 +74,7 @@ export function NewNoteCard() {
               ) : (
                 <textarea
                   autoFocus
+                  value={content}
                   className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                   onChange={handleContentChanged}
                 />
